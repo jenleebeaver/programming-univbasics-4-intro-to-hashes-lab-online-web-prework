917ed47c7220196cc9126a2c60b701755cc36217
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-03 09:33:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,6 @@ end
 
 def update_counting_hash(hash, key)
   return hash[key] # given a hash an a key as parameters, return an updated hash
-  return hash[key: 1]# if the provided key is not present in the hash, add it and assign it to the value of 1
+  return hash["key" => 1]# if the provided key is not present in the hash, add it and assign it to the value of 1
   return hash[key] += 1# if the provided key is present, increment its value by 1
 end
